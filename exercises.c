84eedf227deac1b180e401f4e86277d4da7675f6
Replit user <>
2023-08-18 16:01:19 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -97,16 +97,16 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
 int checkSorted(int arr[], int size) {
-  //int ascendente = 0, descendente = 0; // tipos de orden
+  int ascendente = 0, descendente = 0; // tipos de orden
   for (int i = 0; i < size-1; i++){
     if (arr[i] < arr[i+1]){
-      return 1;
+      ascendente += 1;
     }
     else{
-      return -1;
+      descendente += 1;
     }
   }
-
+  
   
   
   return 0;
