8d1e0ad7a66be28255b3b69c2106e459c8b4e74e
Replit user <>
2023-08-18 15:47:08 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -107,10 +107,10 @@ int checkSorted(int arr[], int size) {
     }
   }
 
-  if(ascendente == size){
+  if(ascendente == size-2){
     return -1;
   }
-  if(descendente == size){
+  if(descendente == size-2){
     return 1;
   }
   
